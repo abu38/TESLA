10f01ed4648dfe6322c48be0d634f78b042f33c1
abu38 <69963310+abu38@users.noreply.github.com>
2020-08-23 06:23:36 +0530

    Emojify

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -131,15 +131,15 @@ async def pipcheck(pip):
 @register(outgoing=True, pattern=r"^.(alive|on)$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    output = ("`My TESLA details `\n"
-              f"`Hi, {DEFAULTUSER} `\n"
+    output = ("`⚡My TESLA details `\n"
+              f"`Hi,👤 {DEFAULTUSER} `\n"
               "==================== \n"
-              f"`Telethon : v{version.__version__} `\n"
-              f"`Python : v{python_version()} `\n"
-              f"`TESLA : {USERBOT_VERSION} `\n"
+              f"`☀️Telethon : v{version.__version__} `\n"
+              f"`🐍Python : v{python_version()} `\n"
+              f"`🥀TESLA : {USERBOT_VERSION} `\n"
               "==================== \n"
-              f"`Running on Branch ==> {UPSTREAM_REPO_BRANCH} `\n"
-              f"Tip:- Type `.help` to know about Modules Information \n")
+              f"`🌍Running on Branch ==> {UPSTREAM_REPO_BRANCH} `\n"
+              f"🔭Tip:- Type `.help` to know about Modules Information \n")
     if ALIVE_LOGO:
         logo = ALIVE_LOGO
         await bot.send_file(alive.chat_id, logo, caption=output)
